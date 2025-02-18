f651ecf2d100063c68846981b540cf2d05bd489c
Kevin Shook <kshook@kshook.ca>
2025-02-18 10:17:56 -0500

    Made code in vignette non-executable to improve installation time on CRAN Graphs are all now .png files

--- vignettes/HGDMr_examples.R
+++ vignettes/HGDMr_examples.R
@@ -40,50 +40,58 @@ small_p <- 1.2
 sub_intervals <- 1
 large_rating <- 1.4
 
-## -----------------------------------------------------------------------------
-simulation <- HGDM(
-  upland_area, 
-  small_depression_area, 
-  large_depression_area, 
-  area_units, 
-  max_small_depression_storage, 
-  max_large_depression_storage,
-  initial_small_depression_storage, 
-  initial_large_depression_storage,
-  storage_units,
-  small_depressions_initial_connected_fraction,
-  upland_fraction_to_small,
-  upland_fraction_to_large,
-  upland_fraction_to_outlet,
-  small_fraction_to_large,
-  forcings = daily_7120951600,
-  small_p,
-  large_rating,
-  sub_intervals)
+## ----eval = FALSE-------------------------------------------------------------
+# simulation <- HGDM(
+#   upland_area,
+#   small_depression_area,
+#   large_depression_area,
+#   area_units,
+#   max_small_depression_storage,
+#   max_large_depression_storage,
+#   initial_small_depression_storage,
+#   initial_large_depression_storage,
+#   storage_units,
+#   small_depressions_initial_connected_fraction,
+#   upland_fraction_to_small,
+#   upland_fraction_to_large,
+#   upland_fraction_to_outlet,
+#   small_fraction_to_large,
+#   forcings = daily_7120951600,
+#   small_p,
+#   large_rating,
+#   sub_intervals)
+
+## ----eval = FALSE-------------------------------------------------------------
+# p <- ggplot(simulation, aes(date, total_outflow_volume)) +
+#   geom_point()
+# p
+
+## ----eval = FALSE-------------------------------------------------------------
+# 
+# p <- ggplot(simulation, aes(date, total_contrib_frac)) +
+#   geom_point() +
+#   ylim(0, 1)
+# p
+
+## ----eval = FALSE-------------------------------------------------------------
+# 
+# simulation$small_depression_water_volume_fraction <-
+#   simulation$small_depression_water_depth / (max_small_depression_storage / 1000)
+# 
+# p2 <- ggplot(simulation,
+#             aes(small_depression_water_volume_fraction,
+#                 small_depression_contrib_frac)) +
+#   geom_point() +
+#   xlab("Meta depression volumetric fraction") +
+#   ylab("Meta depression connected/contributing fraction") +
+#   coord_fixed(ratio = 1) +
+#   xlim(0, 1) +
+#   ylim(0, 1) +
+#   geom_abline(slope = 1, intercept = 0, colour = "red")
+# p2
 
 ## -----------------------------------------------------------------------------
-p <- ggplot(simulation, aes(date, total_contrib_frac)) +
-  geom_point() +
-  ylim(0, 1)
-p
 
-## -----------------------------------------------------------------------------
-simulation$small_depression_water_volume_fraction <- 
-  simulation$small_depression_water_depth / (max_small_depression_storage / 1000)
-
-p2 <- ggplot(simulation, 
-            aes(small_depression_water_volume_fraction, 
-                small_depression_contrib_frac)) +
-  geom_point() +
-  xlab("Meta depression volumetric fraction") +
-  ylab("Meta depression connected/contributing fraction") +
-  coord_fixed(ratio = 1) +
-  xlim(0, 1) +
-  ylim(0, 1) +
-  geom_abline(slope = 1, intercept = 0, colour = "red")
-p2
-
-## -----------------------------------------------------------------------------
 max_large_depression_storage <- 2000
 total_depression_area <- small_depression_area
 large_depression_frac <- 0.3
@@ -97,49 +105,50 @@ upland_fraction_to_large <- 0.02
 upland_fraction_to_outlet <- 0.02
 small_fraction_to_large <- 0.25
 
-## -----------------------------------------------------------------------------
-simulation_large_pond <- HGDM(
-  upland_area, 
-  small_depression_area, 
-  large_depression_area, 
-  area_units, 
-  max_small_depression_storage, 
-  max_large_depression_storage,
-  initial_small_depression_storage, 
-  initial_large_depression_storage,
-  storage_units,
-  small_depressions_initial_connected_fraction,
-  upland_fraction_to_small,
-  upland_fraction_to_large,
-  upland_fraction_to_outlet,
-  small_fraction_to_large,
-  forcings = daily_7120951600,
-  small_p,
-  large_rating,
-  sub_intervals
-  )
+## ----eval = FALSE-------------------------------------------------------------
+# 
+# simulation_large_pond <- HGDM(
+#   upland_area,
+#   small_depression_area,
+#   large_depression_area,
+#   area_units,
+#   max_small_depression_storage,
+#   max_large_depression_storage,
+#   initial_small_depression_storage,
+#   initial_large_depression_storage,
+#   storage_units,
+#   small_depressions_initial_connected_fraction,
+#   upland_fraction_to_small,
+#   upland_fraction_to_large,
+#   upland_fraction_to_outlet,
+#   small_fraction_to_large,
+#   forcings = daily_7120951600,
+#   small_p,
+#   large_rating,
+#   sub_intervals
+#   )
 
-## -----------------------------------------------------------------------------
-p3 <- ggplot(simulation_large_pond, aes(date, total_contrib_frac)) +
-  geom_point()
-p3
+## ----eval = FALSE-------------------------------------------------------------
+# p3 <- ggplot(simulation_large_pond, aes(date, total_contrib_frac)) +
+#   geom_point()
+# p3
 
-## -----------------------------------------------------------------------------
-max_water_volume <- ((max_large_depression_storage / 1000) * 
-                       (large_depression_area * 1e6)) + 
-  ((max_small_depression_storage / 1000) * (small_depression_area * 1e6))
-
-simulation_large_pond$total_water_volume_fraction <-
-  (simulation_large_pond$small_depression_water_volume +
-     simulation_large_pond$large_depression_water_volume) / max_water_volume
-
-p4 <- ggplot(simulation_large_pond, 
-             aes(total_water_volume_fraction, total_contrib_frac)) +
-  geom_point() +
-  xlab("Total volumetric fraction") +
-  ylab("Total connected/contributing fraction") +
-  coord_fixed(ratio = 1) +
-  xlim(0, 1) +
-  ylim(0, 1) 
-p4
+## ----eval = FALSE-------------------------------------------------------------
+# max_water_volume <- ((max_large_depression_storage / 1000) *
+#                        (large_depression_area * 1e6)) +
+#   ((max_small_depression_storage / 1000) * (small_depression_area * 1e6))
+# 
+# simulation_large_pond$total_water_volume_fraction <-
+#   (simulation_large_pond$small_depression_water_volume +
+#      simulation_large_pond$large_depression_water_volume) / max_water_volume
+# 
+# p4 <- ggplot(simulation_large_pond,
+#              aes(total_water_volume_fraction, total_contrib_frac)) +
+#   geom_point() +
+#   xlab("Total volumetric fraction") +
+#   ylab("Total connected/contributing fraction") +
+#   coord_fixed(ratio = 1) +
+#   xlim(0, 1) +
+#   ylim(0, 1)
+# p4
 
